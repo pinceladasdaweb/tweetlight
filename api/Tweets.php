97b304d0615de632eaa571672bc7f9668d5a7610
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-03-03 14:47:49 -0300

    Refactored Tweets class

--- api/Tweets.php
+++ api/Tweets.php
@@ -1,49 +1,39 @@
 <?php
 class Tweets {
-    static public function user_timeline() {
-        // Twitter OAuth library: https://github.com/mynetx/codebird-php
-        require_once ('../codebird.php');
+    public function __construct($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET) {
+        $this->_consumer_key        = $CONSUMER_KEY;
+        $this->_consumer_secret     = $CONSUMER_SECRET;
+        $this->_access_token        = $ACCESS_TOKEN;
+        $this->_access_token_secret = $ACCESS_TOKEN_SECRET;
+    }
 
-        // Twitter OAuth Settings:
-        $CONSUMER_KEY = '';
-        $CONSUMER_SECRET = '';
-        $ACCESS_TOKEN = '';
-        $ACCESS_TOKEN_SECRET = '';
-
-        // Get authenticated:
-        \Codebird\Codebird::setConsumerKey($CONSUMER_KEY, $CONSUMER_SECRET);
+    public function user_timeline() {
+        \Codebird\Codebird::setConsumerKey($this->_consumer_key, $this->_consumer_secret);
         $cb = \Codebird\Codebird::getInstance();
-        $cb->setToken($ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+        $cb->setToken($this->_access_token, $this->_access_token_secret);
 
-        // Retrieve posts:
-        $username = strip_tags(trim($_GET['username']));
-        $count = strip_tags(trim($_GET['count']));
-
-        // API Settings: https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
         $params = array(
-            'screen_name' => $username,
-            'count' => $count
+            'screen_name' => isset($_GET['username']) ? $_GET['username'] : NULL,
+            'count'       => isset($_GET['count']) ? $_GET['count'] : 10
         );
 
-        // Make the REST call:
         $data = (array) $cb->statuses_userTimeline($params);
 
-        unset($data['httpstatus']);
-        unset($data['rate']);
+        return json_encode($data);
+    }
 
-        foreach ($data as $tweet){
-            $tweets[] = array(
-                'username' => $tweet->user->screen_name,
-                'profile_image' => $tweet->user->profile_image_url,
-                'text' => $tweet->text,
-                'created' => $tweet->created_at
-            );
-        }
+    public function hashtag() {
+        \Codebird\Codebird::setConsumerKey($this->_consumer_key, $this->_consumer_secret);
+        $cb = \Codebird\Codebird::getInstance();
+        $cb->setToken($this->_access_token, $this->_access_token_secret);
 
-        // Output result in JSON:
-        return json_encode($tweets);
+        $params = array(
+            'q'     => isset($_GET['hashtag']) ? $_GET['hashtag'] : NULL,
+            'count' => isset($_GET['count']) ? $_GET['count'] : 10
+        );
+
+        $data = (array) $cb->search_tweets($params);
+
+        return json_encode($data);
     }
 }
-
-header('Content-type: application/json');
-echo Tweets::user_timeline();
